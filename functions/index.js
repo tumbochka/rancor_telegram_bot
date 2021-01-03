08e66708c356b84fd06969d47e916ae18aaad8df
lialit <sensaurus@ukr.net>
2021-01-03 16:38:56 +0200

    Firebase app init

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
+
+const Telegraf = require('telegraf');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const bot = new Telegraf(functions.config().telegrambot.key);
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.command('help', ctx => ctx.reply('список команд:\n' +
+    '\n' +
+    '/rancor X[.Y]\n' +
+    '\n' +
+    'внесение % урона в таблицу, где X и Y цифры, разделённые точкой. разрешённый диапазон: 0-30\n' +
+    '\n' +
+    '/rancor ls\n' +
+    '\n' +
+    'просмотр таблицы (ls от list)\n' +
+    '\n' +
+    '/rancor clear\n' +
+    '\n' +
+    'очистка таблицы (происходит автоматически по достижении 110% суммарного урона и после команды call).\n' +
+    '\n' +
+    '/rancor call\n' +
+    '\n' +
+    'принудительное оповещение игроков в таблице о фиксации урона (происходит автоматически по достижении 110% суммарного урона).\n' +
+    '\n' +
+    '/rancor set\n' +
+    '\n' +
+    'настройки'));
+
+bot.command('Мішаня', ctx => ctx.reply('Мішаня - наш лідер!'));
+bot.launch();
+
+exports.bot = functions.https.onRequest((req, res) => {
+    bot.handleUpdate(req.body, res);
+})
